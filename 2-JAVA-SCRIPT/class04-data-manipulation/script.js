66d241e09385a8d0c3db139424a9583bb96800d2
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2024-10-30 20:03:49 -0300

    add fifo and lifo  examples

--- 2-JAVA-SCRIPT/class04-data-manipulation/script.js
+++ 2-JAVA-SCRIPT/class04-data-manipulation/script.js
@@ -40,7 +40,106 @@ console.log(Math.ceil(Math.random() * 10)); // cria números aleatórios entre 1
 console.log(Math.floor(Math.random() * 10)); // cria números aleatórios entre 0 e 9
 console.log(Math.round(Math.random() * 10)); // cria números aleatórios entre 0 e 10
 
-const times = ["Corinthians", "Flamengo", "Vasco", "Fortaleza", "Ceará", "São Paulo"];
+const times = [
+  "Corinthians",
+  "Flamengo",
+  "Vasco",
+  "Fortaleza",
+  "Ceará",
+  "São Paulo",
+];
 
 const randomNumber = Math.floor(Math.random() * times.length);
 console.log(times[randomNumber]);
+
+// manipulação Arrey
+
+const crush = [
+  "Tiazinha",
+  "Rambo",
+  "Bozo",
+  "Paola Oliveira",
+  "Maria Rui Barbosa",
+];
+console.log(crush.length);
+console.log(crush[1]);
+crush[0] = "Feiticeira"; // substituir
+console.log(crush);
+
+let a = "Jeff";
+a[1] = "M"; // String não muda(permanece a primeira)
+console.log(a);
+
+console.log(crush.join()); // separa os valores por virgula ou definir um separador
+console.log("Bem vindo".replace(" ", "-")); //substitue um carac por outro(espaço por -)
+console.log("Bem vindo a aula".replaceAll(" ", "-")); //substitue todos
+
+console.log(crush.includes("Jeff")); // procura se o caractere está na lista
+console.log(crush.includes("Rambo")); // procura se o caractere está na lista
+
+console.log("jefferson Pereira Silva".split(" ")); // fragmenta o texto elimina o caract
+console.log("jefferson Pereira Silva".split(" ")[0]); // fragmenta o texto e paga o caract
+
+console.log(crush.reverse()); // reverte a lista
+console.log("Jefferson".split("").reverse().join("")); // separa, reverte e junta
+
+//função para contar quantas palavras tem
+function wordCount(text) {
+  const count = text.split(" ").length;
+  return count;
+}
+
+const text =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dignissimos eligendi culpa quaerat libero minima sint ipsam dolore optio qui, deleniti error eius explicabo maxime tempore illo! Illo, saepe tenetur!";
+console.log(wordCount(text));
+
+// console.log(
+//   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dignissimos eligendi culpa quaerat libero minima sint ipsam dolore optio qui, deleniti error eius explicabo maxime tempore illo! Illo, saepe tenetur!".split(" ").length);
+
+const tvPrograms = ["Domingo Legal", "Fantastico", "Domingão do huck"];
+
+//LIFO (pilha)
+
+tvPrograms.pop(); // tirar um carac (sempre o ultimo)
+tvPrograms.push("Bom dia e Cia"); // adciona um carac (ultimo)
+tvPrograms.push("Tv Globinho"); // adciona um carac
+
+//FIFO (Fila)
+
+tvPrograms.unshift("TV Cruj"); //trocar o carac (inicio)
+tvPrograms.unshift("Castelo Ratimbum"); //trocar o carac (inicio)
+tvPrograms.shift(); //Apaga ou retira o carac (inicio)
+
+tvPrograms.splice(0, 1); // Apagar (0 onde começa) (1 quantidade de carac apagados)
+tvPrograms.splice(0, 1, "TV teste"); // apaga e adciona (0 onde começa) (1 quantidade de carac apagados) ("Tv teste" adcionado)
+
+console.log(tvPrograms);
+
+// DATE
+
+const dateNow = new Date();
+console.log(dateNow);
+console.log(dateNow.toLocaleString("pt-BR")); // data e hora
+console.log(dateNow.toLocaleDateString("pt-BR")); // data
+console.log(dateNow.toLocaleTimeString("pt-BR")); // hora
+
+console.log(
+  dateNow.toLocaleString("pt-BR", {
+    weekday: "long",
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  })
+);
+
+const birthday = new Date("1994-11-06 00:00:00");
+
+console.log(birthday);
+console.log((dateNow - birthday) / 1000 / 60 / 60 / 24 / 365.25);
+
+console.log(dateNow.getFullYear());
+console.log(birthday.getFullYear());
+console.log(dateNow.getFullYear() - birthday.getFullYear());
