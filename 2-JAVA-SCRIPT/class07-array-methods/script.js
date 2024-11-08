c2f64d97af850292a90d88af7eea3763f15c09d6
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2024-11-08 20:36:26 -0300

    add question

--- 2-JAVA-SCRIPT/class07-array-methods/script.js
+++ 2-JAVA-SCRIPT/class07-array-methods/script.js
@@ -1,5 +1,5 @@
 //
-/// MAP () não muda valor original
+/// MAP (immutable) não muda valor original
 //
 const numbers = [10, 20, 30];
 console.log(numbers.map((num) => num + 10));
@@ -24,3 +24,48 @@ const devs = [
 
 //com aumento
 console.log(devs.map((dev) => dev.salary * 1.1));
+
+// const resp = prompt("Digite o nome: ").toLocaleLowerCase();
+// const resultFilter = devs.filter((dev) => dev.name.toLocaleLowerCase().includes(resp));
+
+// console.log(resultFilter);
+
+// sort
+const numbers2 = [
+  33,
+  1,
+  3,
+  "Zebra",
+  "a",
+  "A",
+  6,
+  2,
+  11,
+  "Emanuel",
+  "emanuel",
+  "Antônio",
+];
+
+console.log(numbers2);
+numbers2.sort();
+numbers2.sort((a, b) => a - b);
+
+console.log(numbers2);
+
+numbers2.sort((a, b) => {
+  if (typeof a == "string" && typeof b == "string") {
+    a = String(a).toLocaleLowerCase();
+    b = String(b).toLocaleLowerCase();
+  }
+
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+});
+
+console.log(numbers2);
+
+// reduce
+const expenses = [20, 32, 38, 220, 50, 100];
+const total = expenses.reduce((a, b) => a + b, 1000);
+console.log(total);
---
+++ script.js
@@ -0,0 +1,111 @@
+// Avaliação JavaScript
+
+// Questão 1 (Data Types):
+
+// Declare as seguntes variáveis e atribua os valores por tipo:
+// Sting, Number, Boolean, Function, Array, Object
+
+const string = "Emanuel";
+let number = 1;
+let boolean = true;
+let addFunction = () => 1 + 1;
+
+function addNumbers() {
+  return 1 + 1;
+}
+
+let array = [1, "A", [1, 2, 3]];
+
+const object = {
+  name: "Emanuel",
+  age: 31,
+};
+
+// Questão 2 (Operators):
+
+// Imprima as operações de soma, subtração, multiplicação, divisão, resto da divisão e potenciação de 2 e 3 e raiz quadrada de 144
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+console.log(2 ** 3);
+console.log(4 % 2);
+console.log(144 ** (1 / 2));
+console.log(Math.sqrt(144));
+
+// Questão 3 (Functions):
+
+// Crie e execute uma função de nome "concat" que retorne "JavaScript" concatenado com um valor passado como argumento e imprima seu retorno
+
+function concat(text) {
+  return "JavaScript." + text;
+}
+console.log(concat("JS"));
+
+// return "JavaScript" + arg;
+
+// Questão 4 (Data Manipulation):
+
+// Usando os métodos FIFO e LIFO, crie um array com 3 valores e posteriormente insira mais um valor no final e substitua o do início por outro
+
+const series = ["Game of Thrones", "Rick and Morty", "Os Simpsons"];
+series.push("The Walking Dead");
+
+series.shift();
+series.unshift("Lá Casa de Papel");
+
+series.splice(0, 1, "Family Guy");
+
+console.log(series);
+
+// Questão 5 (for):
+
+// Liste todos os valores do array criado na questão anterior ultilizando o for
+
+for (let i = 0; i < series.length; i++) {
+  console.log(series[i]);
+}
+
+// Questão 6 (while):
+
+// Escreva os números de 0 a 100 sem os que pertencem à familía do 40, do 50 e do 60
+
+let count = 0; //flag
+while (count < 100) {
+  count++;
+  if (count >= 40 && count <= 69) {
+    continue;
+  } else {
+    console.log(count);
+  }
+}
+
+// Questão 7 (if):
+
+//Crie uma lógica para verificar se o motorista foi multado
+// A infração pode ser por velocidade alta (quando ultrapassar a máxima)
+// E por velocidade baixa (quando estiver inferior à metade da máxima
+// A velocidade da pista é de 60km/h
+
+const speed = 80;
+const max = 60;
+
+if (speed > max) {
+  console.log("O Rachador!");
+} else if (speed < max / 2) {
+  console.log("Pé de Pano");
+} else {
+  console.log("Ré ré ré, ré ré ré");
+}
+
+// Questão 8 (array methods):
+
+//Uma empresa vai dar 10% de aumento para seus funcionários
+//Abaixo tem a lista com todos os salários atuais de cada um deles
+// Apresente os novos salários e o novo total em folha dessa empresa
+
+const salary = [2000, 2800, 3000, 4000, 2200, 7500, 3650, 8000, 2000, 5000];
+
+const NewSalaries = salary.map((salary) => salary * 1.1);
+console.log(NewSalaries);
