f0651471c77b9c4cb3b72773ddb82ce97181b35f
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-01-16 20:31:50 -0300

    add class 05 styles

---
+++ 3-React/class-05-styles/src/App.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export function App() {
+  return (
+    <Container>
+      <h1>Class 05 Styles</h1>;
+    </Container>
+  );
+}
---
+++ 3-React/class-05-styles/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import { Reset } from "./styles/reset.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+    <Reset />
+  </StrictMode>
+);
