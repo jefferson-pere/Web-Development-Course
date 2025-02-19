49145928570776ae83584f0b3ba3bce6e94bb889
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-02-19 18:39:40 -0300

    class-02-notejs

---
+++ 4-Node.js/class-02-routes/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { router } from "./routes";
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}...`);
+});
