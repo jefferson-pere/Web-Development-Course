34880fa171fa04e4eaa1e90f6fba7e0061cdcde6
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-02-24 20:42:25 -0300

    add class 03 zod

--- 4-Node.js/class-03-controllers/src/controllers/user-controllers.ts
+++ 4-Node.js/class-03-controllers/src/controllers/user-controllers.ts
@@ -1,41 +1,46 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/app-error";
+import { z } from "zod";
+
+//zod
+const UserSchema = z.object({
+  name: z.string({ message: "Nome é obrigatório!" }),
+  series: z.string().max(20, "Maximo de 20 caract"),
+  age: z.number(),
+});
 
 export const userControllers = {
-  create(req: Request, res: Response) {
-    const { name, series } = req.body;
+  create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, series, age } = UserSchema.parse(req.body);
 
-    console.log({ name, series });
+      if (!name || !series) {
+        throw new AppError(400, "Todos os dados são obrigatórios!");
+      }
 
-    return res.status(201).json({ message: "user created" });
+      console.log({ name, series });
+      res.status(201).json({ message: "user created" });
+    } catch (error) {
+      next(error);
+    }
   },
+
+  /////
 
   read(req: Request, res: Response) {
     const { id } = req.params;
-    return res.status(200).json({ message: `user ${id} read` });
+    res.status(200).json({ message: `user ${id} read` });
   },
 
   update(req: Request, res: Response) {
     const { id } = req.params;
     const { name, series } = req.body;
-
     console.log({ id, name, series });
-
-    return res.status(200).json({ message: "user update" });
+    res.status(200).json({ message: "user update" });
   },
 
   delete(req: Request, res: Response) {
     const { id } = req.params;
-    return res.status(200).json({ message: `user ${id} deleted` });
+    res.status(200).json({ message: `user ${id} deleted` });
   },
 };
-
-//
-//
-//
-//
-
-// export const userControllers = {
-//   create: (req: Request, res: Response) => {
-//     return res.json({ name: "Class Controllers" });
-//   },
-// };
---
+++ 4-Node.js/class-03-controllers/src/errors/app-errors.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./app-error";
+
+export function appErrors(
+  error: AppError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  console.error(error);
+  res
+    .status(error.status || 400)
+    .json({ message: error.message || "Game Over!" });
+}
